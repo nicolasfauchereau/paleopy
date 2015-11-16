5d9c54b442227c2b3843657d67e8224172d6eb35
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2015-11-16 16:45:58 +1300

    added plot_bar method

--- paleopy/plotting/heatmap.py
+++ paleopy/plotting/heatmap.py
@@ -1,4 +1,10 @@
-def heatmap(df_anoms, df_probs, df_probs_MC): 
+def heatmap(wr):
+    
+    df_anoms = wr.df_anoms
+    df_probs = wr.df_probs
+    df_probs_MC = wr.df_probs_MC
+    classification = wr.classification
+    
     sig90 = make_sig(df_probs, df_probs_MC, sig=90)
     sig95 = make_sig(df_probs, df_probs_MC, sig=95)
     sig99 = make_sig(df_probs, df_probs_MC, sig=99)
@@ -11,30 +17,57 @@ def heatmap(df_anoms, df_probs, df_probs_MC):
     vmin = -1 * vmax
 
     width, height = plt.figaspect(anoms)
-    fig, ax = plt.subplots(figsize=(10, 10))
+    
+    fig, ax = plt.subplots(figsize=(10,10))
+    fig.subplots_adjust(left=0.2, right=0.99)
 
-    #fig = plt.figure(figsize=(12,10))
-    #ax = fig.add_subplot(gs[2::,2::])
 
     X = np.arange(df_anoms.shape[1])
     Y = np.arange(0.5, df_anoms.shape[0] + 0.5)
+
     # im = ax.pcolormesh(X, Y, anoms,vmin=vmin, vmax=vmax, cmap=plt.cm.RdBu_r) 
     im = ax.pcolormesh(anoms,vmin=vmin, vmax=vmax, cmap=plt.cm.RdBu_r) 
+
+    # this is a small adjustment to align the vertical lines to the 
+    # cells boundaries
+#     adj = 0.02
+    adj = 0
 
     for i in range(anoms.shape[0]):
         for j in range(anoms.shape[1]): # maybe not hard code that 
             if sig95[i,j]:
-                ax.plot(j+0.50,i+0.50, "s",  color='k', markersize=11, markerfacecolor='None')
+                ax.plot(j+0.50,i+0.50, "s",  color='k', markersize=11, \
+                        markerfacecolor='None', zorder=1, mew=1.25)
             if sig99[i,j]:
-                ax.plot(j+0.50,i+0.50, "+",  color='k', markersize=11, markerfacecolor='None')
+                ax.plot(j+0.50+adj,i+0.50-adj, "x",  color='k', markersize=10, \
+                        markerfacecolor='None', zorder=1, mew=1.5)
 
     ax.set_yticks(np.arange(0.5, anoms.shape[0] + 0.5))
     ax.set_yticklabels(df_anoms.index[::-1])
 
     ax.set_xticks(np.arange(0.5, anoms.shape[1] + 0.5))
     ax.set_xticklabels(df_anoms.columns)
+    
+    [l.set_fontsize(12) for l in ax.yaxis.get_ticklabels()]
+    [l.set_fontsize(12) for l in ax.xaxis.get_ticklabels()]
 
+    # this is a small adjustment to align the vertical lines to the 
+    # cells boundaries
+#     adj = 0.01
+    adj = 0
+    
+    ax.hlines(np.arange(1,anoms.shape[0]), 0, anoms.shape[1])
+    ax.vlines(np.arange(1-adj,anoms.shape[1]-adj), 0, anoms.shape[0], color='0.1')
+    
+    # remobes the ticks on each axis
+    ax.xaxis.set_ticks_position('none') 
+    ax.yaxis.set_ticks_position('none') 
 
+    # plots the colorbar
     cb = plt.colorbar(im, ticks=np.linspace(vmin, vmax, 11), boundaries=np.linspace(vmin, vmax, 11), drawedges=True)
+    cb.set_label('Frequency anomalies', fontsize=14)
+    [l.set_fontsize(12) for l in cb.ax.yaxis.get_ticklabels()]
+    
+    ax.set_title(classification, fontsize=14)
 
     return fig
---
+++ paleopy/plotting/plot_bar.py
@@ -0,0 +1,46 @@
+def plot_bar(wr, ensemble=True)
+    """
+    """
+    
+    # if the climatological probabilities have 
+    # not been calculated, call the `_get_clim_probs` 
+    # method
+    if not(hasattr(wr, 'clim_probs')): 
+        clim_probs = wr._get_clim_probs()
+    
+    # if the anomalies and the observed 
+    # as well as the MC probabilities have 
+    # not been calculated, call the `probs_anomalies`
+    # method
+    if not(hasattr(wr, 'df_anoms')): 
+        wr.probs_anomalies(kind='many')
+        
+    if wr.df_anoms.shape[1] < 5:
+        print("""
+        not enough proxies in the ensemble, size was {}
+        need at list 5
+        """.format(wr.df_anoms.shape[1]))
+        raise Exception("size error")
+            
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.bar(np.arange(0.5, len(clim_probs)+0.5), clim_probs * 100, color="0.8", width=1., alpha=0.8)
+    ax1.set_ylabel("frequency %")
+    ax2 = ax1.twinx()
+    ax2.axhline(0,color='k', lw=1, linestyle='dashed')
+    ax2.set_ylabel("% change in frequency")
+    
+    df_anoms = wr.df_anoms * 100
+    
+    bp = df_anoms.T.boxplot(ax=ax2, patch_artist=True);
+    
+    wrone = copy.copy(wr)
+    wrone.probs_anomalies(kind='one')
+    
+    for i,b in enumerate(bp['boxes']):
+        if wrone.df_anoms.iloc[i,:].values >= 0: 
+            plt.setp(b,facecolor='red', alpha=0.5)
+        else: 
+            plt.setp(b,facecolor='blue', alpha=0.5)
+    
+    return fig, wrone
