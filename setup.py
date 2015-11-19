a53f25e969a6c2172085e696c9eace62f0bd8f62
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2015-11-20 09:46:09 +1300

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='paleopy',
+      version='1.0',
+      description="""
+      classes and methods to calculates and plot analog composites from
+      a paleoclimate proxy or an ensemble of paleoclimate proxies
+      """,
+      author='Nicolas Fauchereau',
+      author_email='nicolas.fauchereau@gmail.com',
+      packages=['paleopy',],
+     )
