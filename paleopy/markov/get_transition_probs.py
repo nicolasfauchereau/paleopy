320d0cc484d61925502625ef7b93e3bd8a137bef
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2016-04-16 07:02:26 +1200

    started working on HTML tables outputs

--- paleopy/markov/get_transition_probs.py
+++ paleopy/markov/get_transition_probs.py
@@ -25,8 +25,7 @@ def get_transition_probs(x, classes=None):
             ### count the total number of days following the cluster "cl"
             list_freq.append(len(i))
             mat = mat[i,].tolist()
-            ### count the number of occurences of each of the 12 clusters
-            ### TODO (function with N = number of clusters)
+            ### count the number of occurences of each of the N clusters
             list_mat.append([mat.count(c) for c in classes])
         list_mat = np.array(list_mat)
         list_mat = list_mat.sum(0) * 1.0
--- paleopy/plotting/indices.py
+++ paleopy/plotting/indices.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.stats import zscore, ttest_1samp
+from copy import copy
 from ..utils import seasons_params
 
 class indices():
@@ -47,18 +48,29 @@ class indices():
         return self
 
     def composite(self):
+        """
+        composite
+        """
         if not(hasattr(self, 'data')):
             self._read_csv()
+
         if isinstance(self.data, pd.core.frame.DataFrame):
             compos = pd.concat([self.data.ix[str(y)] for y in self.analog_years])
+
         elif isinstance(self.data, dict):
             compos = {}
             for i, k in enumerate(self.data):
                 df = self.data[k]
                 compos[k] = pd.concat([df.ix[str(y)] for y in self.analog_years])
+            list_index = list(compos.keys())
+            df = copy(compos[list_index[0]])
+            for k in list_index[1:]:
+                df.loc[:,k] = compos[k]
+            compos = df
+
         self.compos = compos
 
-    def _plot_df(self, df, ax, ax_n = 0, pval = None):
+    def _plot_df(self, df, ax, ax_n = 0, pval = None, signif = None):
         """
         plots a boxplot for one Series or a DataFrame with
         one column (one index)
@@ -67,31 +79,46 @@ class indices():
         b = df.boxplot(ax = ax, widths=0.85, patch_artist=True)
 
         if df.mean().values < 0:
-            plt.setp(b['boxes'],facecolor='b', edgecolor='b', alpha=0.5)
+            plt.setp(b['boxes'],facecolor='steelblue', edgecolor='steelblue', alpha=0.5)
             plt.setp(b['fliers'], color='gray', marker='+',visible=True)
             plt.setp(b['medians'],color='k',linewidth=1)
             ax.set_ylim(-3, 3)
-            ax.plot(1, df.mean().values, 's', color='b', ms=10)
+            if pval < signif:
+                ax.plot(1, df.mean().values, 's', color='b', ms=10, markeredgewidth=3,\
+                markeredgecolor='k', alpha=0.5)
+            else:
+                ax.plot(1, df.mean().values, 's', color='b', ms=10, alpha=0.5)
+
         else:
-            plt.setp(b['boxes'],facecolor='r', edgecolor='r', alpha=0.5)
+            plt.setp(b['boxes'],facecolor='coral', edgecolor='coral', alpha=0.5)
             plt.setp(b['fliers'], color='gray', marker='+',visible=True)
             plt.setp(b['medians'],color='k',linewidth=1)
             ax.set_ylim(-3, 3)
-            ax.plot(1, df.mean().values, 's', color='r', ms=10)
+            if pval < signif:
+                ax.plot(1, df.mean().values, 's', color='r', ms=10, markeredgewidth=3,\
+                markeredgecolor='k', alpha=0.5)
+            else:
+                ax.plot(1, df.mean().values, 's', color='r', ms=10, alpha=0.5)
+
         ax.axhline(0, c='k')
         if ax_n == 0:
             ax.set_ylabel('std.', fontsize=14)
         [l.set_fontsize(13) for l in ax.xaxis.get_ticklabels()]
         [l.set_fontsize(13) for l in ax.yaxis.get_ticklabels()]
         if pval is not None:
-            ax.set_title("p={:4.2f}\n$\mu$={:4.2f}\n$\sigma$={:4.2f}".format(pval, \
-            df.mean().values[0], \
-            df.std().values[0]), \
-            fontsize=12)
+            if pval < signif:
+                ax.set_title("$p=${:4.2f}\n$\mu=${:4.2f}\n$\sigma=${:4.2f}".format(pval, \
+                df.mean().values[0], \
+                df.std().values[0]), \
+                fontsize=11,
+                fontweight='bold')
+            else:
+                ax.set_title("$p=${:4.2f}\n$\mu=${:4.2f}\n$\sigma=${:4.2f}".format(pval, \
+                df.mean().values[0], \
+                df.std().values[0]), \
+                fontsize=11)
 
-
-
-    def plot(self):
+    def plot(self, signif=0.1):
 
         if not(hasattr(self, 'compos')):
             self.composite()
@@ -108,7 +135,7 @@ class indices():
 
                 t, pval = ttest_1samp(df.values, 0)
 
-                self._plot_df(df, axes[i], ax_n=i, pval=pval[0])
+                self._plot_df(df, axes[i], ax_n=i, pval=pval[0], signif=signif)
 
         elif isinstance(self.compos, dict):
 
@@ -122,6 +149,6 @@ class indices():
 
                 t, pval = ttest_1samp(df.values, 0)
 
-                self._plot_df(df, axes[i], ax_n=i, pval=pval[0])
+                self._plot_df(df, axes[i], ax_n=i, pval=pval[0], signif=signif)
 
         return f
